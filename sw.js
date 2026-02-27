aedd38f25ed4e5fe0df718c1fa8392d51287d3a5
developer.HINOJOSA <140227891+AaronGuzmanSERVICE@users.noreply.github.com>
2026-02-26 20:41:40 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-const cacheName = 'asistencia-cucba-v1';
+const cacheName = 'cucba-qr-v1';
 const assets = [
   './',
   './asistencias.html',
   './asistencias.css',
   './asistencias.js',
-  './Sound/succes.mp3',
-  './Sound/Error.mp3',
-  'https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700;800&display=swap',
+  './ico192.png',
+  './ico512.png',
   'https://unpkg.com/html5-qrcode'
 ];
 
-// Instalación y almacenamiento en caché
+// Instala el Service Worker y guarda los archivos en el celular
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -19,7 +18,7 @@ self.addEventListener('install', e => {
   );
 });
 
-// Estrategia: Primero buscar en caché, si no hay, ir a la red
+// Permite que la app funcione sin internet usando lo guardado
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
